704e07de6d8176369edda6a7dc28dcd3205ed11a
Leonel Machado <mleonel654@gmail.com>
2026-07-18 14:10:10 -0400

    feat: connect return button to the main menu scene

--- places_scene.gd
+++ places_scene.gd
@@ -135,3 +135,8 @@ func _animar_y_esperar_boton(boton: Button) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_back_btn_pressed() -> void:
+	Global.reproducir_tick()
+	get_tree().change_scene_to_file("res://main_menu.tscn")
